b0d3f081fbe66b43599b423a7792abdfa98dda58
Chatura Galagama <chaturangalagama@gmail.com>
2019-09-27 20:37:52 +0530

    removed report-module

--- modules/report/reporting-ui/src/main/java/com/ilt/cms/report/ui/util/BartTemplateNameUtil.java
+++
@@ -1,8 +0,0 @@
-package com.ilt.cms.report.ui.util;
-
-public class BartTemplateNameUtil {
-
-    public static final String TRANSACTION_REPORT = "transaction.rptdesign";
-    public static final String TEST_REPORT = "test.rptdesign";
-
-}
